31769998d9c9ca097ef4a474dbf962d17d4ab00f
ck0146057-dotcom <ck0146057@gmail.com>
2026-03-30 01:14:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ const io = new Server(server);
 // ✅ ENV VARIABLES
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-});
-
 // 🔐 Online users
 const onlineUsers = {};
 
